811bb2b1facfbaee34e60661f28ab2750e756d35
Nitesh Anand <niteshanand23@gmail.com>
2019-04-21 00:44:50 +0000

    Hello program modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,5 @@
-This program will tell us a simple message
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
-
-
-//System.out.println("My Name is Nitesh")
-
-//Adding another line as a part of changes
